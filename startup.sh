c9128eaaf964d8fc48529dfc940d429152fd9432
pBielli <patrizio.bielli1998@gmail.com>
2022-03-21 17:27:30 +0100

    another commit

--- startup.sh
+++
@@ -1,7 +0,0 @@
-# Configure Startup Services File
-echo -e "[\033[0;35mSERVER STARTUP\033[0m]
-loading..."
-service ssh restart
-service vsftpd restart
-#service nodejs restart
-echo -e "[\033[0;32mstarted\033[0m]\n"
