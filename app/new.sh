4e78ae5b81df40221914b67389c125bfe69dd589
pBielli <patrizio.bielli1998@gmail.com>
2022-03-21 17:35:43 +0100

    another commit

--- app/new.sh
+++ app/new.sh
@@ -1,10 +1,7 @@
 # Configure App Creator
-newDir=/home/server/app_$1
-#Generate App Folder and start file
-mkdir -p $newDir
-echo -e "echo '[NODE]$1'\n
-cd /home/server/$1\n
-npm start">> $newDir/start.sh
-chmod 777 $newDir/start.sh
-cd $newDir
-npx create-react-app $1 --template typescript
+APP_NAME=$1
+APPs_DIR=/home/server/apps
+mkdir -p $APPs_DIR
+cd $APPs_DIR
+#Generate app
+npx create-react-app $APP_NAME --template typescript
--- app/start.sh
+++ app/start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Hello World!"
+APP_NAME=$1
+echo "[NODE]$APP_NAME"
+cd /home/server/apps/$APP_NAME
+npm start
