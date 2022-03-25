5f84d4987398c1207188ab08d4795c7812579218
pBielli <patrizio.bielli1998@gmail.com>
2022-03-25 14:58:02 +0100

    cleanup...

--- git/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gitDir=/home/server/pBind
-rm -d -r "${gitDir}"
-git clone https://github.com/pBielli/pBind ${gitDir}
-#remove unused
-rm "${gitDir}/README.md"
-rm -d -r "${gitDir}/server_init"
--- server_init/welcome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -e "\n[ \033[0;36m$1\033[0m ] created 
-  remember to \033[0;31msetup a password\033[0m of tech user
-  enter \033[1;31mpasswd tech \033[0m
-  
-  so.. now..
-  Hello World? or something, i don't know. 
-  \033[0;32mBye! \033[0m\n"
