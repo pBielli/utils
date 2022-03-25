e26736468bc539bd1aff89699920c70477e0f480
pBielli <patrizio.bielli1998@gmail.com>
2022-03-25 19:29:26 +0100

    cleanup...

--- repo/checkVersion.sh
+++ repo/checkVersion.sh
@@ -3,13 +3,13 @@
 
 getValFromJson () {
 return $(grep -o '"$1":"[^"]*' $2 | grep -o '[^"]*$')
-}
+} | 
 key="version"
-val=$(getValFromJson(${key},/home/server/pBind/repo/info.json))
+val=$(getValFromJson ${key} /home/server/pBind/repo/info.json)
 echo "actual:${val}"
 
 remote_file="/home/server/pBind/repo/.tmp_info.json"
-curl "https://raw.githubusercontent.com/pBielli/pBind/master/repo/info.json" ${remote_file}
-val=$(getValFromJson(${key},${remote_file}))
+curl "https://raw.githubusercontent.com/pBielli/pBind/master/repo/info.json" $remote_file
+val=$(getValFromJson $key $remote_file)
 echo "latest:${val}"  
-rm ${remote_file}
+rm $remote_file
