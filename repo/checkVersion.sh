d3450f5f706929ce9f910a32475289036d67ee7e
pBielli <patrizio.bielli1998@gmail.com>
2022-03-25 18:56:15 +0100

    cleanup...

--- repo/checkVersion.sh
+++ repo/checkVersion.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #curl -s "https://raw.githubusercontent.com/pBielli/pBind/master/repo/info.json" .checkInfos
-ssh user@remote_host "cat remote_file.txt" | diff - local_file.txt
+
+getValFromJson (key,file) {
+return $(grep -o '"${key}":"[^"]*' ${file} | grep -o '[^"]*$')
+}
+key="version"
+echo "actual:${getValFromJson(${key},/home/server/pBind/repo/info.json)}"
+echo "latest:${getValFromJson(${key},$(curl https://raw.githubusercontent.com/pBielli/pBind/master/repo/info.json))}"
