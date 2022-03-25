afad3abc4308551d13b75be30cbd35989180177d
pBielli <patrizio.bielli1998@gmail.com>
2022-03-25 19:19:41 +0100

    cleanup...

--- repo/checkVersion.sh
+++ repo/checkVersion.sh
@@ -10,4 +10,4 @@ echo "actual:${val}"
 echo "actual:${getValFromJson(${key},/home/server/pBind/repo/info.json)}"
 curl "https://raw.githubusercontent.com/pBielli/pBind/master/repo/info.json" ${remote_file}
 val=$(getValFromJson(${key},${remote_file}))
-echo "latest:${val}" 
+echo "latest:${val}"  
