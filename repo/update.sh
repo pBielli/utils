1fd91daf053f9bd5386633153f6afd47a0861168
pBielli <patrizio.bielli1998@gmail.com>
2022-03-25 23:13:20 +0100

    update

--- repo/update.sh
+++ repo/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 gitDir=/home/server/pBind
+git clone https://github.com/pBielli/pBind ${gitDir}_NEW
 rm -d -r "${gitDir}"
-git clone https://github.com/pBielli/pBind ${gitDir}
+mv ${gitDir}_NEW ${gitDir}
+
 #apply config (remove unused) 
 bash "${gitDir}/configurations/cleanup.sh"
