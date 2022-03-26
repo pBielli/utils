22dc7f5ded93c4ede4c80789ac43b62d958836b9
Patrizio Bielli <40484128+pBielli@users.noreply.github.com>
2022-03-26 15:25:38 +0100

    Update docker_container.sh

--- configurations/init/docker_container.sh
+++ configurations/init/docker_container.sh
@@ -2,4 +2,4 @@
 #echo -e "\n[\033[0;35mHello World!\033[0m]\ni'm working on this..."
 curl -s -t Dockerfile "https://raw.githubusercontent.com/pBielli/pBind/master/configurations/Dockerfile"
 docker build --tag="pb_node_server" .
-docker run --hostname=pb_node_server --name=pb_node_server pb_node_server
+docker run -t -i --hostname=pb_node_server --name=pb_node_server pb_node_server
