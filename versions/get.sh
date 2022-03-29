cd5b899267928d1f973ad4fa083c4376bacc6d21
pBielli <patrizio.bielli1998@gmail.com>
2022-03-29 18:56:10 +0200

    update

--- versions/get.sh
+++ versions/get.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+#clear
 #apt-get install jq -y
 #apt-get install subversion -y
 
 #Graphic Functions
-function hr () { 
-echo -e "  ______________________\n" 
+function hr () {
+echo -e "  ______________________\n"
 }
 function list_el () {
 echo -e "  [$1] $2"
@@ -12,31 +13,39 @@ echo -e "  [$1] $2"
 
 #Logic functions
 function getVersion () {
-curl https://raw.githubusercontent.com/pBielli/pBind/master/versions/list.json | jq -c ".[] | select( .type==\"$1\").name"
+curl -s https://raw.githubusercontent.com/pBielli/pBind/master/versions/list.json | jq -c ".[] | select( .type==\"$1\").version"
 }
+function getInfoFile () {
+curl  "https://raw.githubusercontent.com/pBielli/pBind/master/versions/list/${1}/info.json"
+}
+
 
 #Preparing the list of versions
 all_versions=""
-versions=("1.0","beta")
+versions=("1.0" "beta")
 for index in "${!versions[@]}"
 do
-	version=$(curl "https://raw.githubusercontent.com/pBielli/pBind/master/versions/list/${versions[index]}/info.json" | jq -c ".version")
-    if [ $index == 0];then
-	 all_versions="$(list_el $index $version)"
-	else
-	 all_versions="${all_versions}\n$(list_el $index $version)"
-	fi
+        infoF=$( getInfoFile "${versions[index]}" )
+        echo $info
+        echo "${versions[index]}"
+        echo "https://raw.githubusercontent.com/pBielli/pBind/master/versions/list/${versions[index]}/info.json"
+                version=$( $info | jq -c ".version")
+    if [ $index == 0 ];then
+         all_versions="$(list_el $index $version)"
+        else
+         all_versions="${all_versions}\n$(list_el $index $version)"
+        fi
 done
 stable=$(getVersion stable)
-latest=$(getVersion latest)
 beta=$(getVersion beta)
+#latest=$(getVersion latest)
+
 
 while [[ ! $CH =~ ^[0-3]{1} ]]; do
 echo " [VERSION]"
 list_el 0 "custom"
 list_el 1 "stable:${stable}"
-list_el 2 "latest:${latest}"
-list_el 3 "beta:  ${beta}"
+list_el 2 "beta:  ${beta}"
 echo -e "\n${all_versions}"
 hr
 
@@ -54,16 +63,16 @@ if [ $CH == 1 ];then
  VERSION="$stable"
 fi
 if [ $CH == 2 ];then
- VERSION="$stable"
+ VERSION="$beta"
 fi
-if [ $CH == 3 ];then
- VERSION="$stable"
-fi
-if (($CH >= 4 && $CH < (4+${#versions[@]})  ));then
- i=$(($CH-4))
+first_i=3
+if (($CH >= $first_i && $CH < ($first_i+${#versions[@]})  ));then
+ i=$(($CH-$first_i))
  VERSION="${versions[i]}"
 fi
 
+echo "VERSION:${VERSION}"
+##################################################################################
 source /etc/environment
 
 #else, or after it, download the repo
@@ -71,8 +80,8 @@ svn export https://github.com/pBielli/pBind/trunk/versions/$VERSION $PBIND_PATH
 
 if [ -z ${PBIND_PATH+x}];then
 #unset, so install it completelly
-	bash $(find "pBind/" -name "install.sh")
-	bash $(find "pBind/" -name "setup.sh")
+        bash $(find "pBind/" -name "install.sh")
+        bash $(find "pBind/" -name "setup.sh")
 fi
 
-bash $(find "pBind/" -name "purge.sh")
+bash $(find "pBind/" -name "purge.sh")                           
