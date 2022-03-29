ce5d3ddcfabe07a6cc9b71faf2da9067a6fdbf15
pBielli <patrizio.bielli1998@gmail.com>
2022-03-29 19:16:39 +0200

    update

--- versions/get.sh
+++ versions/get.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#clear
+clear
+echo "Loading..."
 #apt-get install jq -y
 #apt-get install subversion -y
 
@@ -16,40 +17,42 @@ function getVersion () {
 curl -s https://raw.githubusercontent.com/pBielli/pBind/master/versions/list.json | jq -c ".[] | select( .type==\"$1\").version"
 }
 function getInfoFile () {
-curl  "https://raw.githubusercontent.com/pBielli/pBind/master/versions/list/${1}/info.json"
+curl -s "https://raw.githubusercontent.com/pBielli/pBind/master/versions/list/${1}/info.json"
 }
 
 
 #Preparing the list of versions
 all_versions=""
 versions=("1.0" "beta")
+first_i=3
+last_i=$(($first_i+${#versions[@]}))
 for index in "${!versions[@]}"
 do
-        infoF=$( getInfoFile "${versions[index]}" )
-        echo $info
-        echo "${versions[index]}"
-        echo "https://raw.githubusercontent.com/pBielli/pBind/master/versions/list/${versions[index]}/info.json"
-                version=$( $info | jq -c ".version")
-    if [ $index == 0 ];then
-         all_versions="$(list_el $index $version)"
-        else
-         all_versions="${all_versions}\n$(list_el $index $version)"
-        fi
+        info=$( getInfoFile "${versions[index]}" )
+        version=$( echo $info | jq -c ".version")
+
+    if [ $index > 0 ];then
+         all_versions="${all_versions}\n"
+    fi
+    all_versions="${all_versions}$(list_el $(($index+$first_i)) $version)"
+
+
 done
 stable=$(getVersion stable)
 beta=$(getVersion beta)
 #latest=$(getVersion latest)
 
 
-while [[ ! $CH =~ ^[0-3]{1} ]]; do
+while [[ ! $CH =~ ^[0-$last_i]{1} ]]; do
+clear
 echo " [VERSION]"
 list_el 0 "custom"
 list_el 1 "stable:${stable}"
 list_el 2 "beta:  ${beta}"
-echo -e "\n${all_versions}"
+echo -e "${all_versions}"
 hr
 
-read -n 1 -p "  selection: " CH
+read -p "  selection: " CH
 echo ""
 
 if [ $CH == 0 ];then
@@ -65,18 +68,18 @@ fi
 if [ $CH == 2 ];then
  VERSION="$beta"
 fi
-first_i=3
-if (($CH >= $first_i && $CH < ($first_i+${#versions[@]})  ));then
+if (($CH >= $first_i && $CH < $last_i));then
  i=$(($CH-$first_i))
  VERSION="${versions[i]}"
 fi
 
 echo "VERSION:${VERSION}"
+
 ##################################################################################
 source /etc/environment
 
 #else, or after it, download the repo
-svn export https://github.com/pBielli/pBind/trunk/versions/$VERSION $PBIND_PATH
+svn export https://github.com/pBielli/pBind/trunk/versions/list/$VERSION $PBIND_PATH
 
 if [ -z ${PBIND_PATH+x}];then
 #unset, so install it completelly
